3cc9086cfdd8cb9ef5f329e0114529615295a670
ToulAura <102886021+M1cki0g@users.noreply.github.com>
2024-11-16 11:17:04 +0100

    Create header.h

---
+++ header.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class etudiant{
+string nom;
+float nt1;
+float nt2;
+
+public:
+    float calc_moy();
+    etudiant(float,float,string);
+    void afficher(float);
+
+
+};
